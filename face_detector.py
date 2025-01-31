fd292c77224ed7e80344e346a542c21de446f1d3
francescaguzzi <fguzzi99@gmail.com>
2025-01-31 21:20:36 +0100

    versione sequenziale istogramma

--- face_detector.py
+++ face_detector.py
@@ -8,6 +8,80 @@ import time
 import dlib
 import cv2
 
+# ------------------------------------ #
+
+def calculate_histograms(image, histograms, w, h):
+
+    # nel caso sequenziale, consideriamo l'intera immagine
+    for y in range(h):
+        for x in range(w):
+            idx = y * w + x  # indice del pixel nell'immagine
+            pixel_value = image[idx]
+            histograms[pixel_value] += 1 
+
+
+# ------------------------------------ #
+
+def calculate_cdfs(histograms, cdfs, clipLimit):
+
+    # clipping dell'istogramma
+    excess = 0
+    if clipLimit > 0:
+        for i in range(256):
+            excess += max(histograms[i] - clipLimit, 0)
+            histograms[i] = min(histograms[i], clipLimit)
+        
+        # ridistribuzione dell'eccesso
+        binIncr = excess // 256
+        
+        for i in range(256):
+            histograms[i] += binIncr
+        
+        if excess > 0:
+            stepSz = 1 + (excess // 256)
+            for i in range(256):
+                if excess <= 0:
+                    break
+                excess -= stepSz
+                histograms[i] += stepSz
+    
+    # calcolo delle CDF
+    cdfs[0] = histograms[0]
+    for i in range(1, 256):
+        cdfs[i] = histograms[i] + cdfs[i - 1]
+
+# ------------------------------------ #
+
+def apply_cdfs(image, outputImage, cdfs, w, h):
+
+    for y in range(h):
+        for x in range(w):
+            idx = y * w + x
+            pixelValue = image[idx]
+            equalizedPixel = (cdfs[pixelValue] - cdfs[0]) * 255 // (w * h - cdfs[0])
+            outputImage[idx] = np.uint8(equalizedPixel)
+
+# ------------------------------------ #
+
+def histogram_equalization_sequential(input_image):
+
+    height, width = input_image.shape
+
+    histograms = np.zeros(256, dtype=np.int32)
+    cdfs = np.zeros(256, dtype=np.int32)
+
+    calculate_histograms(input_image.flatten(), histograms, width, height)
+
+    clipLimit = 40
+    calculate_cdfs(histograms, cdfs, clipLimit)
+
+    output_image = np.empty_like(input_image)
+    apply_cdfs(input_image.flatten(), output_image.flatten(), cdfs, width, height)
+
+    return output_image
+
+
+# ------------------------------------ #
 
 def eye_aspect_ratio(eye):
 
@@ -18,13 +92,12 @@ def eye_aspect_ratio(eye):
     ear = (A + B) / (2.0 * C)
     return ear
 
-
+# ------------------------------------ #
 
 def facial_recognition(orientation, blink, lock):
 
-    # Initialize constants for EAR threshold and consecutive frames
-    EYE_AR_THRESH = 0.2 
-    EYE_AR_CONSEC_FRAMES = 3
+    EYE_AR_THRESH = 0.2
+    EYE_AR_CONSEC_FRAMES = 3 # frame consecutivi in cui l'occhio deve rimanere chiuso 
 
     COUNTER = 0
     TOTAL = 0
@@ -42,12 +115,14 @@ def facial_recognition(orientation, blink, lock):
     while True:
 
         frame = vs.read()
-        frame = cv2.flip(frame, 1)  # mirrors image
+        frame = cv2.flip(frame, 1)  
 
         frame = imutils.resize(frame, width=600)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        rects = detector(gray, 0)
+        enhanced_gray = histogram_equalization_sequential(gray)
+
+        rects = detector(enhanced_gray, 0)
 
         ear = 0
         orientation_value = 0 # 0 centro, 1 destra, 2 sinistra
@@ -56,30 +131,26 @@ def facial_recognition(orientation, blink, lock):
 
         for rect in rects:
 
-            # determine the facial landmarks for the face region, then
-            # convert the facial landmark (x, y)-coordinates to a NumPy
-            # array
-            shape = predictor(gray, rect)
+            # estrazione dei facial landmarks 
+            shape = predictor(enhanced_gray, rect)
             shape = face_utils.shape_to_np(shape)
 
-            # extract the left and right eye coordinates, then use the
-            # coordinates to compute the eye aspect ratio for both eyes
+            # estrazione delle coordinate degli occhi e calcolo EAR
             leftEye = shape[lStart:lEnd]
             rightEye = shape[rStart:rEnd]
 
-            # average the eye aspect ratio together for both eyes
+            # media dell'EAR per entrambi gli occhi
             leftEAR = eye_aspect_ratio(leftEye)
             rightEAR = eye_aspect_ratio(rightEye)
             ear = (leftEAR + rightEAR) / 2.0
 
-            # compute the convex hull for the left and right eye, then
-            # visualize each of the eyes
+            # display del contorno degli occhi
             leftEyeHull = cv2.convexHull(leftEye)
             rightEyeHull = cv2.convexHull(rightEye)
             cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
             cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-            # Check if the eyes are closed (blink detection)
+            # blink detection
             if ear < EYE_AR_THRESH:
                 COUNTER += 1
             else:
@@ -108,14 +179,14 @@ def facial_recognition(orientation, blink, lock):
             orientation.value = orientation_value
             blink.value = blink_event
         
-        # Display the blink count
-        cv2.putText(frame, f"Blinks: {TOTAL}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, f"Orientamento: {orientation_value}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
-        
+        cv2.putText(enhanced_gray, f"Blinks: {TOTAL}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.putText(enhanced_gray, "EAR: {:.2f}".format(ear), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(enhanced_gray, f"Orientamento: {orientation_value}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
-        cv2.imshow("Frame", frame)
+
+        # cv2.imshow("Frame", frame)
+        cv2.imshow("Histogram Equalization", enhanced_gray)
         key = cv2.waitKey(1) & 0xFF
 
         if key == ord("q"):
--- face_detectorCUDA.py
+++ face_detectorCUDA.py
@@ -14,18 +14,121 @@ from pycuda.compiler import SourceModule
 
 # ------------------------------------ #
 
-with open("cuda_kernels.ptx", "r") as f:
-    ptx_code = f.read()
+# KERNEL CUDA #
+
+# kernel calcolo degli istogrammi
+
+mod_hi = SourceModule("""
+                   
+__global__ void calculate_histograms(unsigned char *image, unsigned int *histograms, unsigned int w, unsigned int h) {   
+
+                   
+    //ogni blocco corrisponde ad un pixel, un thread all'interno del blocco per un pixel della finestra
+                               
+    //indice del blocco
+    unsigned int bid = blockIdx.y * gridDim.x + blockIdx.x ;
+                   
+    //indice all'interno della finestra
+    //unsigned int bid = threadIdx.y * blockDim.x + threadIdx.x ;
+                   
+    //indice globale
+    // unsigned int tid = bid * (blockDim.x * blockDim.y) + bid ;
+                   
+    //indice di scorrimento dell'immagine
+    unsigned int idx = bid + threadIdx.x + threadIdx.y * gridDim.x;
+        
+    if ( idx < w*h ){
+        //viene aggiornato il counter per il livello di luminosità di image[idx] della finestra di indice bid
+        atomicAdd( &(histograms[(bid*256)+image[idx]]) , 1 ) ;
+    }
+}
 
 
-mod = cuda.module_from_buffer(ptx_code)
+""")
 
-calculate_histogram = mod.get_function("calculate_histogram")
-calculate_cdfs = mod.get_function("calculate_cdfs")
-apply_cdfs = mod.get_function("apply_cdfs")
+# kernel clipping e calcolo delle cdf 
 
+mod_cdf = SourceModule("""
+
+__global__ void calculate_cdfs(unsigned int *histograms, int *cdfs, unsigned int clipLimit) {
+
+    //un thread per finestra               
+    unsigned idx = threadIdx.x + blockIdx.x*blockDim.x;                   
+
+    //clipping degli istogrammi
+    int excess = 0 ; 
+     
+    if ( clipLimit > 0 ) {
+            for ( unsigned int i = 0 ; i < 256 ; i++ ) {
+            unsigned int excess = histograms[(idx*256)+i] - clipLimit ;
+            if (excess > 0) { excess+=excess; }
+        }    
+
+        unsigned int binIncr = (int)(excess / 256) ;
+        unsigned int upper = clipLimit - binIncr ;
+                       
+        //distribuzione degli eccessi
+        for ( unsigned int i = 0 ; i < 256 ; i++ ) {
+            if (histograms[(idx*256)+i] > clipLimit) { 
+                histograms[(idx*256)+i] = clipLimit; 
+            } else {
+                excess -= binIncr ;
+                histograms[(idx*256)+i] += binIncr ;
+            }
+        }  
+
+        if ( excess > 0 ) {
+            //ridistribuisco l'eccesso
+            unsigned int stepSz = 1 + (int)(excess / 256) ;
+            for ( unsigned int i = 0 ; i < 256 && excess > 0; i++ ) {
+                excess -= stepSz ; 
+                histograms[(idx*256)+i] += stepSz;
+            }                   
+        }
+    }   
+                       
+    //calcolo della cdf
+    cdfs[idx*256] = histograms[idx*256] ;            
+    
+    for ( unsigned int i = 1 ; i < 256 ; i++ ) {
+        cdfs[(idx*256)+i] = histograms[(idx*256)+ i ] + cdfs[(idx*256)+ i - 1] ;
+    }
+}
+        
+""")
+
+# kernel applicazione cdf
+
+mod_apply = SourceModule("""
+                   
+__global__ void apply_cdfs(unsigned char *image, unsigned char *outputImage, int *cdfs, unsigned int w, unsigned int h) {   
+
+                   
+    //ogni blocco corrisponde ad una finestra, un thread all'interno del blocco per un pixel della finestra
+                               
+    //indice del blocco
+    unsigned int bid = blockIdx.y * gridDim.x + blockIdx.x ;
+                   
+    //indice all'interno del blocco
+    unsigned int lid = threadIdx.y * blockDim.x + threadIdx.x ;
+                   
+    //indice globale
+    // unsigned int tid = bid * (blockDim.x * blockDim.y) + bid ;
+                   
+    //indice di scorrimento dell'immagine
+    unsigned int idx = blockIdx.y * ((blockDim.y * blockDim.x) * gridDim.x ) + threadIdx.y * blockDim.x * gridDim.x + blockIdx.x * blockDim.x + threadIdx.x ;
+                                         
+    
+    if ( idx < w*h ){
+        int pixelValue = image[idx];
+        int equalizedPixel = (cdfs[(idx*256)+pixelValue] - cdfs[idx*256]) * 255 / ((blockDim.x * blockDim.y) - cdfs[idx*256]);
+        outputImage[idx] = static_cast<unsigned char>(equalizedPixel);
+    }
+}
+""")
 
 # ------------------------------------ #
+
 
 def histogram_equalization_cuda(input_image):
 
@@ -63,7 +166,7 @@ def histogram_equalization_cuda(input_image):
 
     # lancio del kernel per calcolare gli istogrammi
     starttime = time.time()
-    # calculate_histogram = mod_hi.get_function("calculate_histograms")
+    calculate_histogram = mod_hi.get_function("calculate_histograms")
     calculate_histogram(d_image, d_histograms, np.int32(width), np.int32(height), block=(blockDimX, blockDimY, 1), grid=(width, height))
     endtime = time.time()
     elapsed = endtime - starttime
@@ -81,7 +184,7 @@ def histogram_equalization_cuda(input_image):
     nrThreads = 512 # ogni SN può contenere 512x3 e ogni blocco 1024 -> occupancy al massimo
     blockSplit = (width * height) // 512
 
-    # calculate_cdfs = mod_cdf.get_function("calculate_cdfs")
+    calculate_cdfs = mod_cdf.get_function("calculate_cdfs")
     starttime1 = time.time()
     calculate_cdfs(d_histograms, d_cdfs, np.int32(clipLimit), block=(nrThreads, 1, 1), grid=(blockSplit, 1))
     endtime1 = time.time()
@@ -97,7 +200,7 @@ def histogram_equalization_cuda(input_image):
 
     # Kernel per applicare l'equalizzazione dell'istogramma
     starttime = time.time()
-    # apply_cdfs = mod_apply.get_function("apply_cdfs")
+    apply_cdfs = mod_apply.get_function("apply_cdfs")
     apply_cdfs(d_image, d_output_image, d_cdfs, np.int32(width), np.int32(height), block=(blockDimX, blockDimY, 1), grid=(width//blockDimX, height//blockDimY))
     endtime = time.time()
     elapsed = endtime - starttime
@@ -112,12 +215,11 @@ def histogram_equalization_cuda(input_image):
     d_output_image.free()
     d_histograms.free()
     d_cdfs.free()
+
     #necessario???? 
     return output_image
 
 # ------------------------------------ #
-
-
 
 def eye_aspect_ratio(eye):
 
@@ -128,7 +230,7 @@ def eye_aspect_ratio(eye):
     ear = (A + B) / (2.0 * C)
     return ear
 
-
+# ------------------------------------ #
 
 def facial_recognition(orientation, blink, lock):
 
